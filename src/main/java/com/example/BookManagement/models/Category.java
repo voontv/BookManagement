80302d2e901035d1e1916b9972866ef24d815d8b
voontv <voontv@gmail.com>
2019-04-24 19:55:42 +0700

    add model category

--- src/main/java/com/example/BookManagement/models/Category.java
+++ src/main/java/com/example/BookManagement/models/Category.java
@@ -1,14 +1,26 @@
 package com.example.BookManagement.models;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Category {
     @Id
-    int id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NonNull
+    private int id;
 
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "Name is mandatory")
+    private String name;
 }
