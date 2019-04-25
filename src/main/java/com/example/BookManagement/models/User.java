45a5b5e43b19b92c5ffcee9fb45e2103a78052ef
voontv <voontv@gmail.com>
2019-04-25 15:51:37 +0700

    try fixed

--- src/main/java/com/example/BookManagement/models/User.java
+++ src/main/java/com/example/BookManagement/models/User.java
@@ -7,7 +7,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
--- src/test/java/com/example/BookManagement/BookManagementApplicationTests.java
+++ src/test/java/com/example/BookManagement/BookManagementApplicationTests.java
@@ -17,6 +17,7 @@ public class BookManagementApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("Test");
 	}
 
 }
