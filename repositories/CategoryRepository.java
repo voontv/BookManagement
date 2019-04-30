07668d2d815c200757e9e9f76c88b4e39df5adce
voontv <voontv@gmail.com>
2019-04-30 21:19:39 +0700

    delete file redundancy

--- repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BookManagement.repositories;
-
-import com.example.BookManagement.models.dao.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
