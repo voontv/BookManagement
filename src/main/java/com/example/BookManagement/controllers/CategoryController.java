33f4a625cc0b6d6ffdada08789920d20c47a0da0
voontv <voontv@gmail.com>
2019-05-01 08:44:57 +0700

    created feature category api

---
+++ src/main/java/com/example/BookManagement/controllers/CategoryController.java
@@ -0,0 +1,68 @@
+package com.example.BookManagement.controllers;
+
+import com.example.BookManagement.converters.bases.Converter;
+import com.example.BookManagement.exceptions.NotFoundException;
+import com.example.BookManagement.models.dao.Category;
+import com.example.BookManagement.models.dto.CategoryDTO;
+import com.example.BookManagement.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private Converter<Category, CategoryDTO> categoryCategoryDTOConverter;
+
+    @Autowired
+    private Converter<CategoryDTO, Category> categoryDTOCategoryConverter;
+
+    @GetMapping()
+    List<CategoryDTO> get() {
+        return categoryCategoryDTOConverter.convert(categoryRepository.findAll(Sort.by("id")));
+    }
+
+    @GetMapping("/{id}")
+    CategoryDTO get(@PathVariable int id) {
+
+        if(categoryRepository.existsById(id))
+        {
+            return categoryCategoryDTOConverter.convert(categoryRepository.findById(id).get());
+        } else {
+            throw new NotFoundException(String.format("Category have id %d not found",id));
+        }
+    }
+
+    @PostMapping()
+    void post(@RequestBody CategoryDTO categoryDTO) {
+        categoryDTO.setId(0);
+        categoryRepository.save(categoryDTOCategoryConverter.convert(categoryDTO));
+    }
+
+    @PutMapping
+    void put(@RequestBody CategoryDTO categoryDTO) {
+
+        if(!categoryRepository.findById(categoryDTO.getId()).isPresent()) {
+            throw new NotFoundException(String.format("Category have id %d not found",categoryDTO.getId()));
+        } else {
+            categoryRepository.save(categoryDTOCategoryConverter.convert(categoryDTO));
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable int id) {
+
+        if(categoryRepository.existsById(id)) {
+            categoryRepository.deleteById(id);
+        } else {
+            throw new NotFoundException(String.format("Category have id %d not found",id));
+        }
+    }
+}
